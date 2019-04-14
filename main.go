4688f1adec28766235f41522fa8d778fda70370a
Zach Deibert <zachdeibert@gmail.com>
2019-04-14 18:01:36 -0500

    Fixed client ID

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/rand"
+	"time"
 
 	"./sources"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
@@ -10,12 +11,13 @@ import (
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	opts := MQTT.NewClientOptions().AddBroker("tcp://127.0.0.1:1883")
 	cs := make([]byte, 23-len("rocketui"))
 	for i := range cs {
 		cs[i] = alphabet[rand.Int63()%int64(len(alphabet))]
 	}
-	opts.SetClientID(string(cs))
+	opts.SetClientID("rocketui" + string(cs))
 	c := MQTT.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
